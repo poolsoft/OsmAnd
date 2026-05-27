486d9c7ede6d649f377b5a5606c2332b8cbac305
poolsoft <muhammedgulacti@kahramanmaras.bel.tr>
2026-05-27 23:11:05 +0300

    Fix compiler error in WidgetPanelFragment by importing android.content.Intent

--- OsmAnd/src-carlauncher/java/net/osmand/plus/carlauncher/ui/WidgetPanelFragment.java
+++ OsmAnd/src-carlauncher/java/net/osmand/plus/carlauncher/ui/WidgetPanelFragment.java
@@ -1,6 +1,7 @@
 package net.osmand.plus.carlauncher.ui;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
