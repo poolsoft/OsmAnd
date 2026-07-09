a2d39abdf67fbc458280b73efaeeb0d7fdf74f1c
poolsoft <muhammedgulacti@kahramanmaras.bel.tr>
2026-07-10 00:18:01 +0300

    Fix missing Context import in CarLauncherSettingsFragment

--- OsmAnd/src-carlauncher/java/net/osmand/plus/carlauncher/ui/CarLauncherSettingsFragment.java
+++ OsmAnd/src-carlauncher/java/net/osmand/plus/carlauncher/ui/CarLauncherSettingsFragment.java
@@ -1,6 +1,7 @@
 package net.osmand.plus.carlauncher.ui;
 
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
